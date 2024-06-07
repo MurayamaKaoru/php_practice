6cff5334865b1d088b30a1b43fc428c87e2ba793
MurayamaKaoru <s1robudo0918@gmail.com>
2024-06-07 15:06:43 +0900

    練習問題１の解答を記入

--- php-practice_1.php
+++ php-practice_1.php
@@ -1,39 +1,187 @@
 <?php
 // Q1 変数と文字列
+$name = '村山';
+
+echo '私の名前は『' .$name. '』です。';
 
 // Q2 四則演算
+$num = 5*4;
+
+echo $num ."\n";
+echo $num/2;
 
 // Q3 日付操作
+$timestamp = strtotime('2019-5-23 14:48:22');
 
+echo '現在時刻は、' .date('Y年m月d日 H時i分s秒', $timestamp). 'です。';
 
 // Q4 条件分岐-1 if文
+$device = 'mac';
 
+if ($device === 'windows') {
+  echo '使用OSは、です。';
+  return;
+} if ($device === 'mac') {
+  echo '使用OSは、です。';
+} else {
+  echo 'どちらでもありません。';
+}
 
 // Q5 条件分岐-2 三項演算子
+$age = 19;
 
+$message = ($age < 18)? '未成年です。' : '成人です。';
+echo $message
 
 // Q6 配列
+$prefecture = ['東京都', '千葉県', '埼玉県', '茨城県', '神奈川県', '栃木県', '群馬県', ];
 
+echo $prefecture[2]. 'と' .$prefecture[3]. 'は関東地方の都道府県です。';
 
 // Q7 連想配列-1
+$prefecture =[
+  '東京都' => '新宿区',
+  '神奈川県' => '横浜市',
+  '千葉県' => '千葉市',
+  '埼玉県' => 'さいたま市',
+  '栃木県' => '宇都宮市',
+  '群馬県' => '前橋市',
+  '茨城県' => '水戸市'
+];
 
+echo
+$prefecture['東京都']."\n",
+$prefecture['神奈川県']."\n",
+$prefecture['千葉県']."\n",
+$prefecture['埼玉県']."\n",
+$prefecture['栃木県']."\n",
+$prefecture['群馬県']."\n",
+$prefecture['茨城県'];
 
 // Q8 連想配列-2
+$prefecture =[
+  '東京都' => '新宿区',
+  '神奈川県' => '横浜市',
+  '千葉県' => '千葉市',
+  '埼玉県' => 'さいたま市',
+  '栃木県' => '宇都宮市',
+  '群馬県' => '前橋市',
+  '茨城県' => '水戸市'
+];
+
+foreach ($prefecture as $key => $value){
+  if ($value == 'さいたま市') {
+      echo $key. 'の県庁所在地は、' .$value. 'です。';
+  }
+}
 
 
 // Q9 連想配列-3
+$prefecture =[
+  '東京都' => '新宿区',
+  '神奈川県' => '横浜市',
+  '千葉県' => '千葉市',
+  '埼玉県' => 'さいたま市',
+  '栃木県' => '宇都宮市',
+  '群馬県' => '前橋市',
+  '茨城県' => '水戸市',
+  '北海道' => '札幌市',
+  '沖縄県' => '那覇市'
+];
 
+foreach ($prefecture as $key => $value){
+  if ($value == '新宿区') {
+    echo $key. 'の県庁所在地は、' .$value. 'です。' . "\n";
+    continue;
+  }
+  if ($value == '横浜市') {
+    echo $key. 'の県庁所在地は、' .$value. 'です。' . "\n";
+    continue;
+  }
+  if ($value == '千葉市') {
+    echo $key. 'の県庁所在地は、' .$value. 'です。' . "\n";
+    continue;
+  }
+  if ($value == 'さいたま市') {
+    echo $key. 'の県庁所在地は、' .$value. 'です。' . "\n";
+    continue;
+  }
+  if ($value == '宇都宮市') {
+    echo $key. 'の県庁所在地は、' .$value. 'です。' . "\n";
+    continue;
+  }
+  if ($value == '前橋市') {
+    echo $key. 'の県庁所在地は、' .$value. 'です。' . "\n";
+    continue;
+  }
+  if ($value == '水戸市') {
+    echo $key. 'の県庁所在地は、' .$value. 'です。' . "\n";
+    continue;
+  }
+  else {
+      echo $key. 'は関東地方ではありません。' . "\n";
+  }
+}
 
 // Q10 関数-1
+function sayHello($name)
+{
+  echo $name. 'さん、こんいちは。'."\n";
+}
 
+sayHello(かおる);
+sayHello(あすか);
 
 // Q11 関数-2
+function calcTaxInPrice($price)
+{
+  $taxInPrice = $price*1.1;
 
+  echo $price. '円の商品の税込価格は' .$taxInPrice. '円です。';
+}
+
+calcTaxINPrice(1000);
 
 // Q12 関数とif文
+function distinguishNum($num)
+  {
+    if ($num % 2 == 0) {
+    echo $num. 'は偶数です。'."\n";
+  }
+  else {
+    echo $num. 'は奇数です。'."\n";
+  }
+}
 
+distinguishNum(211);
+distinguishNum(256);
 
 // Q13 関数とswitch文
+function evaluateGrade($grade)
+{
+  switch ($grade) {
+    
+    case 'A':
+    case 'B':
+      echo '合格です。'."\n";
+      break;
+
+    case 'C':
+      echo '合格ですが追加課題があります。'."\n";
+      break;
+
+    case 'D':
+      echo '不合格です。'."\n";
+
+    default:
+      echo '判定不明です。講師に問い合わせてください。'."\n";
+        break;
+  }
+}
+
+evaluateGrade('B');
+evaluateGrade('小峠英二');
+
 
 
 ?>
--- php-practice_2.php
+++ php-practice_2.php
@@ -1,15 +1,126 @@
 <?php
 // Q1 tic-tac問題
+echo '1から100までのカウントを始めます。'."\n";
+echo "\n";
 
+for ($num = 1; $num <= 100; $num++) {
+  switch ($num)
+  {
+    case $num % 4 == 0 && $num % 5 == 0:
+      echo 'tic-tac'."\n";
+      break;
+      
+    case $num % 4 == 0:
+      echo 'tic'."\n";
+      break;
+
+    case $num % 5 == 0:
+      echo 'tac'."\n";
+      break;
+
+    default:
+      echo $num."\n";
+      break;
+  }
+}
 
 // Q2 多次元連想配列
+$personalInfos = [
+  [
+      'name' => 'Aさん',
+      'mail' => 'aaa@mail.com',
+      'tel'  => '09011112222'
+  ],
+  [
+      'name' => 'Bさん',
+      'mail' => 'bbb@mail.com',
+      'tel'  => '08033334444'
+  ],
+  [
+      'name' => 'Cさん',
+      'mail' => 'ccc@mail.com',
+      'tel'  => '09055556666'
+  ],
+];
 
+// 上記の配列を用いて下記のように表示してください。
+// 出力結果
+// Bさんの電話番号は08033334444です。
+echo $personalInfos[1]['name']. 'の電話番号は' .$personalInfos[1]['tel']. 'です。';
+
+// oreachを用いて下記のように表示してください。(数字が1から始まっていることに注意しましょう)
+// 出力結果
+// 1番目のAさんのメールアドレスはaaa@mail.comで、電話番号は09011112222です。
+// 2番目のBさんのメールアドレスはbbb@mail.comで、電話番号は08033334444です。
+// 3番目のCさんのメールアドレスはccc@mail.comで、電話番号は09055556666です。
+foreach ($personalInfos as $i => $value) {
+  echo $i++. '番目の' .$value['name']. 'のメールアドレスは' $value['mail']. 'で、電話番号は' .$value['tel']. 'です。';
+}
+
+// $ageList = [25, 30, 18];
+// 上記の$ageListを使用して、$personalinfosに age というKeyに対して$ageListのそれぞれの年齢をValueとして追加してください。その際は、foreachを使用してください。
+// 追加ができたらvar_dumpを使用して配列の中身を確認してください。
+$ageList = [25, 30, 18];
+
+foreach ($personalInfos as $i => &$key) {{
+  $key['age'] = $ageList[$i];
+}}
+
+var_dump($personalInfos);
 
 // Q3 オブジェクト-1
+class Student
+{
+    public $studentId;
+    public $studentName;
 
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend()
+    {
+        echo '授業に出席しました。';
+    }
+}
+
+$yamada = new Student(120, '山田');
+echo '学籍番号' .$yamada->studentId. '番の生徒は' .$yamada->studentName. 'です。';
 
 // Q4 オブジェクト-2
+// 山田はPHPの授業に参加しました。学籍番号：120
+class Student
+{
+    public $studentId;
+    public $studentName;
 
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend($language)
+    {
+        echo $this->studentName. 'は' .$language. 'の授業に参加しました。学籍番号：' .$this->studentId;
+    }
+}
+
+$yamada = new Student(120, '山田');
+$yamada->attend('PHP');
 
 // Q5 定義済みクラス
+// 1ヶ月前の日付を表すDateTimeインスタンスを作成し、出力結果と同じフォーマットで出力しましょう。
+$onemonthAgo = new DateTime('2021-02-02');
+echo $onemonthAgo->format('Y-m-d');
+
+// 今日の日付と1992年4月25日との日付の差を計算して、総日数を出力しましょう。
+$oldDate = new DateTime('1992-4-25');
+$nowDate = new DateTime();
+
+$diffDate = $oldDate->diff($nowDate);
+echo 'あの日から' .$diffDate->format('%a'). '日経過しました。';
+
 ?>
